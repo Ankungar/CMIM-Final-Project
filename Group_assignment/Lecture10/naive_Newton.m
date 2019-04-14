ba40201a2a66c7cb9f33d1ac74506885c957348c
Ankungar <48430336+Ankungar@users.noreply.github.com>
2019-04-14 19:11:24 +0300

    Delete naive_Newton.m

--- Group_assignment/Lecture10/naive_Newton.m
+++
@@ -1,8 +0,0 @@
-function result = naive_Newton(f,dfdx,starting_value,tol)
-    x = starting_value;
-    while abs(f(x)) > tol
-        x = x - f(x)/dfdx(x);
-        disp(x);
-    end
-    result = x;
-end
